6cc35131886b91ac67548c1ed2642e6c146f2095
mfaizadya <m.faiz.adya@gmail.com>
2025-05-16 18:14:53 +0700

    Menambah soal.json untuk diintegrasi pada KelolaSoal.cs

---
+++ TubesKPL/Soal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubesKPL
+{
+    public enum JenisSoal { Esai, PilihanGanda}
+    public class Soal
+    {
+        public int Id { get; set; }
+        public JenisSoal Jenis { get; set; }
+        public string Pertanyaan { get; set; }
+        public string Jawaban { get; set; }
+        public List<string>? Opsi { get; set; }
+    }
+}
