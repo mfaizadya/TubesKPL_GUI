5b27267e2f2f37f14a32d48baa95ce4c72f49c8e
RakaValrizqy <pendragonqword@gmail.com>
2025-05-16 15:24:28 +0700

    Model Pelajar dan Admin

---
+++ AuthAPI/Pelajar.cs
@@ -0,0 +1,36 @@
+﻿namespace AuthAPI
+{
+    public class Pelajar
+    {
+        private string nama { get; set; }
+        private string username { get; set; }
+        private string password { get; set; }
+
+        public Pelajar() { }
+
+        public Pelajar(string nama, string username, string password)
+        {
+            this.nama = nama;
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {+
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
