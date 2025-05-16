cc1af035ac3aaf1a54cda918110077cbedb50068
Rakunberacun <mrakan711@gmail.com>
2025-05-16 17:09:47 +0700

    menambahkan label pada MenuPelajar

--- TubesKPL/MenuPelajar.Designer.cs
+++ TubesKPL/MenuPelajar.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MenuPelajar";
+            labelNama = new Label();
+            SuspendLayout();
+            // 
+            // labelNama
+            // 
+            labelNama.AutoSize = true;
+            labelNama.Location = new Point(47, 35);
+            labelNama.Name = "labelNama";
+            labelNama.Size = new Size(133, 32);
+            labelNama.TabIndex = 0;
+            labelNama.Text = "LabelNama";
+            // 
+            // MenuPelajar
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(labelNama);
+            Name = "MenuPelajar";
+            Text = "MenuPelajar";
+            Load += MenuPelajar_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelNama;
     }
 }
