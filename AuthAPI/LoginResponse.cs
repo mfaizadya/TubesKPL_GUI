9c27a2e6ba56b8de25f68e2897dd865794bd6fe4
RakaValrizqy <pendragonqword@gmail.com>
2025-05-16 15:30:07 +0700

    Model LoginReq dan LoginResponse

---
+++ AuthAPI/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthAPI
+{
+    public class LoginResponse
+    {
+        public string nama { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
